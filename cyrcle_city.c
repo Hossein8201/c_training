2522075128a77a7f93d4886f9622bf503bc00651
hossein tatar <161024752+Hossein8201@users.noreply.github.com>
2024-03-22 02:46:35 +0330

    untraked files

---
+++ cyrcle_city.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(){
+    int m,n,street[1000000],in=0,out=0;
+    // char cyrcle[1000000];
+    scanf("%d %d",&n,&m);
+    for (int i=0;i<n;i++){
+        scanf("%d ",& street[i]);
+        // if (street[i]==1)   out =1;
+        // else in=1;
+    }
+    // scanf("%s",cyrcle);
+    // if (in == out && out ==1)  printf("YES");
+    // else  printf("NO");
+}
--- zini.c
+++ zini.c
@@ -5,6 +5,6 @@ int main(){
     int m,n;
     int string[1000000][1000000];
     scanf("%d %d",&n,&m);
-    for (int i=0;i<n;i++){for (int j=0;j<m;j++)   scanf("%d ",& string[i][j]);}   
-    for (int i=0;i<n;i++){for (int j=0;j<m;j++)   printf("%d ",string[i][j]);}   
+    // for (int i=0;i<n;i++){for (int j=0;j<m;j++)   scanf("%d ",& string[i][j]);}   
+    // for (int i=0;i<n;i++){for (int j=0;j<m;j++)   printf("%d ",string[i][j]);}   
 }
