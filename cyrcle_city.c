5d2f9c93e2ec1ecbad9c3f70ebbc3f9095aca96f
hossein tatar <161024752+Hossein8201@users.noreply.github.com>
2024-03-22 10:19:30 +0330

    untraked files

--- cyrcle_city.c
+++ cyrcle_city.c
@@ -1,15 +1,13 @@
 #include <stdio.h>
-#include <string.h>
 
 int main(){
-    char street[1000000];
-    int n,m,in=0,out=0;
+    int street,n,m,in=0,out=0;
     char cyrcle[1000000];
     scanf("%d %d",& n,& m);
-    for (int i=0;i<n+1;i++){
-        scanf("%c ",& street[i]);
-        if (street[i]== '1')   out =1;
-        else if (street[i] == '0') in=1;
+    for (int i=0;i<n;i++){
+        scanf("%d ",& street);
+        if (street== 1)   out =1;
+        else if (street== 0)   in=1;
     }
     scanf("%s",cyrcle);
     if (in == out && out ==1)  printf("YES");
