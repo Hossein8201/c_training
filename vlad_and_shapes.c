84cd8b362bb1017cc68630f5b1950e7810e2dcd6
hossein tatar <161024752+Hossein8201@users.noreply.github.com>
2024-03-27 00:38:42 +0330

    a defecult problem from CODEFORCE site

--- vlad_and_shapes.c
+++ vlad_and_shapes.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <string.h>
-
+//This is a defecult problem
 int main(){
     int n,t,bet=0;
     char chart[11][11],answer[1000][1000]={""};
@@ -14,7 +14,6 @@ int main(){
             scanf("%c ",&chart[j][t-1]);
         }
         for(int k=0;k<t;k++)    scanf("%c",&chart[t-1][k]);
-
         for(int j=0;j<t;j++){   for(int k=0;k<t;k++){   
             if(chart[j][k]=='1' && bet==0){
                 if(chart[j][k+1]=='1' && chart[j+1][k]=='1' && chart[j+1][k+1]=='1')    strcat(answer[i],"SQUARE");
