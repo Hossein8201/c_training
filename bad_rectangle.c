ab6dedac0b3f1c98568d2dca3b3a34f9541d573e
hossein tatar <161024752+Hossein8201@users.noreply.github.com>
2024-03-29 10:47:05 +0330

    untraked files

--- bad_rectangle.c
+++ bad_rectangle.c
@@ -3,10 +3,16 @@
 #define DIFF(a,b,c) (((a)+(b)>(c) ? (1) : (0)))
 
 int main(){
-    int sum,a,b,c;
+    int sum,a,b,c,jj=-1,n=0;
     scanf("%d",&sum);
-    if(sum%3==2){   a=floor(sum/3);     b=ceil(sum/3);     c=ceil(sum/3);}
-    else{    a=floor(sum/3);     b=floor(sum/3);     c=ceil(sum/3);}
-    
-
+    a=floor(sum/5);   b=floor(sum/5);     c=sum-a-b;
+    for(int i=0;i<=ceil(0.2*sum);i++){
+        jj+=1;
+        for(int j=jj;j<=ceil(0.2*sum);j++){
+            int    A=a+i,B=b+j,C=sum-A-B;
+            if(A<B && DIFF(A,B,C) && DIFF(A,C,B) && DIFF(B,C,A)){  printf("%d %d %d\n",A,B,C);     n+=1;}
+            else if(A==B && A==C && DIFF(A,B,C) && DIFF(A,C,B) && DIFF(B,C,A)){    printf("%d %d %d\n",A,B,C);     n+=1;}
+        }
+    }
+    printf("%d",n);
 }
