dff99f0d188d60ec4cb3e3b58a2a9d699cdab3f3
hossein tatar <161024752+Hossein8201@users.noreply.github.com>
2024-03-30 02:26:57 +0330

    untraked files

--- bad_rectangle.c
+++ bad_rectangle.c
@@ -6,12 +6,12 @@ int main(){
     int sum,a,b,c,jj=-1,n=0;
     scanf("%d",&sum);
     a=floor(sum/5);   b=floor(sum/5);     c=sum-a-b;
-    for(int i=0;i<=ceil(0.2*sum);i++){
-        jj+=1;
-        for(int j=jj;j<=ceil(0.2*sum);j++){
-            int    A=a+i,B=b+j,C=sum-A-B;
-            if(A<B && DIFF(A,B,C) && DIFF(A,C,B) && DIFF(B,C,A)){  printf("%d %d %d\n",A,B,C);     n+=1;}
-            else if(A==B && A==C && DIFF(A,B,C) && DIFF(A,C,B) && DIFF(B,C,A)){    printf("%d %d %d\n",A,B,C);     n+=1;}
+    for(int i=a;i<=floor(sum/3);i++){
+        jj=a+1;
+        for(int j=jj;j<=ceil(sum/2);j++){
+            int     C=sum-i-j;
+            if(i<j && DIFF(i,j,C) && DIFF(i,C,j) && DIFF(j,C,i)){    printf("%d %d %d\n",i,j,C);     n+=1;}
+            else if(i==j && i==C && DIFF(i,j,C) && DIFF(i,C,j) && DIFF(j,C,i)){    printf("%d %d %d\n",i,j,C);     n+=1;}
         }
     }
     printf("%d",n);
