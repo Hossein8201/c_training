79f85c662abce4c0757b795335539a16628df2a0
hossein tatar <161024752+Hossein8201@users.noreply.github.com>
2024-04-01 17:59:56 +0330

    +

--- graph.c
+++ graph.c
@@ -1,15 +1,8 @@
 #include <stdio.h>
 #include <math.h>
 
-int factorial(int a){
-    int result=1;
-    for(int i=2;i<=a;i++)  result*=i;
-    return result; 
-}
-
 int main(){
-    int n;
-    scanf("%d",&n);
-    if(n==1)    printf("0");
-    else    printf("%d",factorial(n)/(factorial(n-2)*2));
+    long long int n;
+    scanf("%lld",&n);
+    printf("%lld",(n*(n-1))/2);
 }
