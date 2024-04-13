70c7a3b7198285c8e404bfb06210fd73f352d47f
HosseinTatar <161024752+Hossein8201@users.noreply.github.com>
2024-04-13 18:46:02 +0330

    Add files via upload

---
+++ c_tasks_workshop.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+#include <math.h>
+#define PI 3.1415
+
+int main()
+{
+    // the test for sinus and mark of cosinus
+    // double angle,sinus,cosinus;
+    // char dcosinus,dsinus,mark;
+    // scanf("%lf %c",&angle,&mark);
+    // angle *= (PI/180);
+    // sinus= sin(angle);
+    // if(sinus>0 && sinus<1)      dsinus='+';     else if(sinus>-1 && sinus<0)    dsinus='-';
+    // cosinus = cos(angle);
+    // if(cosinus>0 && cosinus<1)      dcosinus='+';     else if(cosinus>-1 && cosinus<0)    dcosinus='-';
+    // if(mark==dcosinus){
+    //     if(dsinus=='+' && dcosinus == '+')   printf("one");
+    //     else if(dsinus=='+' && dcosinus == '-')   printf("two");
+    //     else if(dsinus=='-' && dcosinus == '-')   printf("three");
+    //     else if(dsinus=='-' && dcosinus == '+')   printf("four");
+    // }
+    // else    printf("wrong input!");
+
+
+    // the task for calculator
+    // float number1,number2;
+    // char operation;
+    // printf("please print your numbers: ");
+    // scanf("%f %f",&number1,&number2);
+    // printf("choose your operation you want: ");
+    // scanf(" %c",&operation);
+    // switch(operation){
+    //     case '+':   
+    //         printf("your calculation is: %f %c %f = %f",number1,operation,number2,number1+number2);
+    //         break;
+    //     case '-':
+    //         printf("your calculation is: %f %c %f = %f",number1,operation,number2,number1-number2);
+    //         break;
+    //     case '*':
+    //         printf("your calculation is: %f %c %f = %f",number1,operation,number2,number1*number2);
+    //         break;
+    //     case '/':
+    //         printf("your calculation is: %f %c %f = %f",number1,operation,number2,number1/number2);
+    //         break;
+    //     default:
+    //         printf("your operation is not define!");
+    // }
+
+    // the task for ...
+    // int destination;
+    // scanf("%d",& destination);
+    // switch(destination%4){
+    //     case 0:     printf("%d %d",destination*(-1)/4,destination/4);   break;
+    //     case 1:     destination-=1;    printf("%d %d",-1*destination/4,-1*destination/4);  break;
+    //     case 2:     destination-=2;    printf("%d %d",destination/4+1,-1*destination/4);   break;
+    //     case 3:     destination-=3;    printf("%d %d",destination/4+1,destination/4+1);    break;
+    // }
+}
