b8ae0347844cadd25026ac042e543dcaf52a2f65
hossein tatar <161024752+Hossein8201@users.noreply.github.com>
2024-04-15 17:28:00 +0330

    M

--- c_tasks_workshop.c
+++ c_tasks_workshop.c
@@ -2,47 +2,46 @@
 #include <math.h>
 #define PI 3.1415
 
-int main()
-{
-    // the test for sinus and mark of cosinus
-    // double angle,sinus,cosinus;
-    // char dcosinus,dsinus,mark;
-    // scanf("%lf %c",&angle,&mark);
-    // angle *= (PI/180);
-    // sinus= sin(angle);
-    // if(sinus>0 && sinus<1)      dsinus='+';     else if(sinus>-1 && sinus<0)    dsinus='-';
-    // cosinus = cos(angle);
-    // if(cosinus>0 && cosinus<1)      dcosinus='+';     else if(cosinus>-1 && cosinus<0)    dcosinus='-';
-    // if(mark==dcosinus){
-    //     if(dsinus=='+' && dcosinus == '+')   printf("one");
-    //     else if(dsinus=='+' && dcosinus == '-')   printf("two");
-    //     else if(dsinus=='-' && dcosinus == '-')   printf("three");
-    //     else if(dsinus=='-' && dcosinus == '+')   printf("four");
-    // }
-    // else    printf("wrong input!");
+int main(){
+    //the test for sinus and mark of cosinus
+    double angle,sinus,cosinus;
+    char dcosinus,dsinus,mark;
+    scanf("%lf %c",&angle,&mark);
+    angle *= (PI/180);
+    sinus= sin(angle);
+    if(sinus>0 && sinus<1)      dsinus='+';     else if(sinus>-1 && sinus<0)    dsinus='-';
+    cosinus = cos(angle);
+    if(cosinus>0 && cosinus<1)      dcosinus='+';     else if(cosinus>-1 && cosinus<0)    dcosinus='-';
+    if(mark==dcosinus){
+        if(dsinus=='+' && dcosinus == '+')   printf("one\n");
+        else if(dsinus=='+' && dcosinus == '-')   printf("two\n");
+        else if(dsinus=='-' && dcosinus == '-')   printf("three\n");
+        else if(dsinus=='-' && dcosinus == '+')   printf("four\n");
+    }
+    else    printf("wrong input!\n");
 
 
-    // the task for calculator
-    // float number1,number2;
-    // char operation;
-    // printf("please print your numbers: ");
-    // scanf("%f %f",&number1,&number2);
-    // printf("choose your operation you want: ");
-    // scanf(" %c",&operation);
-    // switch(operation){
-    //     case '+':   
-    //         printf("your calculation is: %f %c %f = %f",number1,operation,number2,number1+number2);
-    //         break;
-    //     case '-':
-    //         printf("your calculation is: %f %c %f = %f",number1,operation,number2,number1-number2);
-    //         break;
-    //     case '*':
-    //         printf("your calculation is: %f %c %f = %f",number1,operation,number2,number1*number2);
-    //         break;
-    //     case '/':
-    //         printf("your calculation is: %f %c %f = %f",number1,operation,number2,number1/number2);
-    //         break;
-    //     default:
-    //         printf("your operation is not define!");
-    // }
+    //the task for calculator
+    float number1,number2;
+    char operation;
+    printf("please print your numbers: ");
+    scanf("%f %f",&number1,&number2);
+    printf("choose your operation you want: ");
+    scanf(" %c",&operation);
+    switch(operation){
+        case '+':   
+            printf("your calculation is: %f %c %f = %f",number1,operation,number2,number1+number2);
+            break;
+        case '-':
+            printf("your calculation is: %f %c %f = %f",number1,operation,number2,number1-number2);
+            break;
+        case '*':
+            printf("your calculation is: %f %c %f = %f",number1,operation,number2,number1*number2);
+            break;
+        case '/':
+            printf("your calculation is: %f %c %f = %f",number1,operation,number2,number1/number2);
+            break;
+        default:
+            printf("your operation is not define!");
+    }
 }
