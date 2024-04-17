207f3c31c7e3d43ad388e4d1c642177bea09d710
hossein tatar <161024752+Hossein8201@users.noreply.github.com>
2024-04-17 22:35:46 +0330

    gitignore

--- kase_va_nokhod.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-
-int main(){
-    int n,transform[10000][2];
-    scanf("%d",&n);
-    for (int i=0;i<n;i++)  scanf("%d %d",&transform[i][1],&transform[i][2]);
-    printf("%d",transform[n-1][2]);
-}
