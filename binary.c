e648814a563841363352a5a87b6ed0e1cf2651a5
hossein tatar <161024752+Hossein8201@users.noreply.github.com>
2024-04-19 03:22:30 +0330

    M

--- binary.c
+++ binary.c
@@ -1,17 +1,12 @@
 #include <stdio.h>
 
 int main(){
-    // char string[100001];
-    // int flag=1;
-    // scanf("%s",string);
-    // for(int i=0;i<strlen(string);i++){
-    //     if(string[i]=='0' && flag)      continue;
-    //     else{printf("%c",string[i]);    flag=0;}
-    // }
-    // if(flag)    printf("0");
-
-    // another way:
-    long long int n;
-    scanf("%lld",&n);
-    printf("%lld",n);
+    char string[100001];
+    int flag=1;
+    scanf("%s",string);
+    for(int i=0;i<strlen(string);i++){
+        if(string[i]=='0' && flag)      continue;
+        else{printf("%c",string[i]);    flag=0;}
+    }
+    if(flag)    printf("0");
 }
--- kase_va_nokhod.c
+++ kase_va_nokhod.c
@@ -1,8 +1,12 @@
 #include <stdio.h>
 
 int main(){
-    int n,transform[10000][2];
+    int n,kase1,kase2,start=1;
     scanf("%d",&n);
-    for (int i=0;i<n;i++)  scanf("%d %d",&transform[i][1],&transform[i][2]);
-    printf("%d",transform[n-1][2]);
+    for(int i=0;i<n;i++){
+        scanf("%d %d",&kase1,&kase2);
+        if(kase1==start)     start=kase2;
+        else if(kase2==start)      start=kase1;
+    }
+    printf("%d",start);
 }
