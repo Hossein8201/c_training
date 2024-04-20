7f11765331b72f3e6c51d4486bbc518f1d9944c6
hossein tatar <161024752+Hossein8201@users.noreply.github.com>
2024-04-21 02:41:38 +0330

    -

--- eraser.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <math.h>
-
-int main(){
-    double m,n,a,b;
-    scanf("%lf %lf %lf %lf",&n,&m,&a,&b);
-    if(n >= m){     if(a >= b)      printf("%.0lf",ceil(m/a));     else    printf("%.0lf",ceil(m/b));}
-    else{       if(a >= b)      printf("%.0lf",ceil(n/a));     else    printf("%.0lf",ceil(n/b));}
-}
--- schedule_Nima.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-
-int main(){
-    int w,s,i;
-    scanf("%d %d %d",&w,&s,&i);
-    printf("%d",24-w-s+i);
-}
